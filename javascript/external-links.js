4028f29b52369638d08de1a63ffbfaa8bf12e9ce
Jeremie Jarosh <jeremie@jarosh.org>
2015-01-24 14:12:22 -0600

    Open all external links in a new window

--- javascript/external-links.js
+++ javascript/external-links.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	console.log("working");
+	/**
+	 * Open all external links in a new window
+	 */
+	$('a').each(function(){
+		var $elm = $(this),
+			host = window.location.host,
+			href = $elm.attr('href'),
+			regex = new RegExp("^(http(s)?:)?//(?!"+host+")", 'i');
+			
+		if ( href.match(regex) ) {
+		//	console.log("external",href);
+			$elm.addClass("external-link").attr("target","_blank");
+		//} else {
+		//	console.log("internal",href);
+		}
+	});
 });
